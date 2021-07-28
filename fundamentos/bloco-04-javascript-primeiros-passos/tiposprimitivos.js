4a98c5d85c3b3e44632b84c551f83b4dfddffea2
thais menezes <thaismenezesd@hotmail.com>
2021-07-28 18:17:36 -0300

    primeiros passos com js

---
+++ fundamentos/bloco-04-javascript-primeiros-passos/tiposprimitivos.js
@@ -0,0 +1,11 @@
+let base = 5;
+
+let altura = 8;
+
+let area = 40;
+
+console.log(base * altura);
+
+let perimetro = 80;
+
+console.log(40 + 40);
---
+++ fundamentos/bloco-04-javascript-primeiros-passos/variaveis.js
@@ -0,0 +1,13 @@
+const myName = "Thais";
+
+const birthCity = "Santos";
+birthCity = "São Paulo"
+
+let birthYear = 1996;
+birthYear = 2030;
+
+console.log(myName);
+
+console.log(birthCity);
+
+console.log(birthYear);
