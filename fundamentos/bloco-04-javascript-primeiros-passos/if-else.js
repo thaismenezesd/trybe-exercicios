f334b8159068aa6c7ebb478f157167eeee3a598f
thais menezes <thaismenezesd@hotmail.com>
2021-07-28 20:28:35 -0300

    adiciona exercicio if/else

--- fundamentos/bloco-04-javascript-primeiros-passos/if-else.js
+++ fundamentos/bloco-04-javascript-primeiros-passos/if-else.js
@@ -1,9 +1,19 @@
 let nota = 80;
 
 if (nota >= 80) {
-    console.log("Parabéns, você foi aprovada(o)");
+console.log("Parabéns, você foi aprovada(o)");
 } else if (nota < 80 && nota >= 60) {
-    console.log ("Você está na nossa lista de espera");
-} else if (nota < 60) {
-    console.log("Você foi reprovada(o)");
+console.log("Você está na nossa lista de espera");
+} else {
+console.log("Você foi reprovada(o)");
+}
+
+nota = 50;
+
+if (nota >= 50) {
+console.log("Parabéns, você foi aprovada(o)");
+} else if (nota < 50 && nota >= 40) {
+console.log("Você está na nossa lista de espera");
+} else {
+console.log("Você foi reprovada(o)");
 }
