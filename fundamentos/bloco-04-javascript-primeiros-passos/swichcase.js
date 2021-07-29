b567b980b03f4e593eb8b8ce2123add49844d198
thais menezes <thaismenezesd@hotmail.com>
2021-07-29 08:55:51 -0300

    exercicio swich case

---
+++ fundamentos/bloco-04-javascript-primeiros-passos/swichcase.js
@@ -0,0 +1,18 @@
+let nota = "Nada";
+
+switch (nota) {
+    case "Aprovada(o)":
+    console.log("Parabéns, você foi aprovada(o)");
+    break;
+
+    case "Lista":
+    console.log("Você está na nossa lista de espera");
+    break;
+
+    case "Reprovada":
+    console.log("Você foi reprovada(o)");
+    break;
+
+    default:
+    console.log("Valor não identificado");
+}
