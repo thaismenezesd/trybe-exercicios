93bde1c759bee341e1ae75a09b92cb951e4bb3a4
thais menezes <thaismenezesd@hotmail.com>
2021-08-02 19:25:55 -0300

    retornando msg

--- fundamentos/bloco-04-javascript-primeiros-passos/objetos-funcoes.js
+++ fundamentos/bloco-04-javascript-primeiros-passos/objetos-funcoes.js
@@ -37,3 +37,13 @@ let names = {
  for (let key in car) {
     console.log(key, car[key]);
  } 
+
+ //Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
+
+ let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  console.log('Bem vinda,' + ' ' + info.personagem);
