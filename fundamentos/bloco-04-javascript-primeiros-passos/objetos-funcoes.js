416f15864173cab282c0b958e107400af75b5c91
thais menezes <thaismenezesd@hotmail.com>
2021-08-02 19:01:17 -0300

    executando chaves e valores dentro do objeto

--- fundamentos/bloco-04-javascript-primeiros-passos/objetos-funcoes.js
+++ fundamentos/bloco-04-javascript-primeiros-passos/objetos-funcoes.js
@@ -25,3 +25,15 @@ let names = {
      console.log('Olá' + ' ' + names['person2']);
      console.log('Olá' + ' ' + names['person3']);
  }
+
+ //Imprima um console.log com as chaves e valores desse objeto.
+
+ let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+
+ for (let key in car) {
+    console.log(key, car[key]);
+ } 
