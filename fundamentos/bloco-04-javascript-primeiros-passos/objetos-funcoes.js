131bdf9548adb6634d74097175ac74fd3bf62f38
thais menezes <thaismenezesd@hotmail.com>
2021-08-03 11:40:12 -0300

    retorna menor valor na funçao

--- fundamentos/bloco-04-javascript-primeiros-passos/objetos-funcoes.js
+++ fundamentos/bloco-04-javascript-primeiros-passos/objetos-funcoes.js
@@ -87,4 +87,16 @@ let names = {
   
   console.log(indiceDoMaior([2, 3, 6, 7, 10, 1]));
 
+  //função que receba um array de inteiros e retorne o índice do menor valor.
+
+  function indiceDoMenor(numeros) {
+    let indiceMenor = 0;
+    for (let indice in numeros) {
+      if (numeros[indiceMenor] > numeros[indice]) {
+        indiceMenor = indice;
+      }
+    }
+    return indiceMenor;
+  }
   
+  console.log(indiceDoMenor([2, 4, 6, 7, 10, 0, -3]));
